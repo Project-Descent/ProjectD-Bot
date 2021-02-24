833ea4856e8bac2aa01ce1d34dbcec2fb3568572
b2af <35238594+b2af@users.noreply.github.com>
2021-02-23 19:35:48 -0500

    Add files via upload

---
+++ Commands.cs
@@ -0,0 +1,114 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
+
+namespace TutorialBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("say")]
+        [Summary("Echo text.")]
+        public async Task SayAsync([Remainder] string messageString = null)
+        { 
+            if (String.IsNullOrEmpty(messageString)) return;
+            await ReplyAsync(messageString);
+        }
+
+        [Command("report")]
+        [Summary("report games from DM")]
+        public async Task SendPMAsync([Remainder] string messageString = "Who did you play with?")
+        {
+            if (String.IsNullOrEmpty(messageString)) return;
+            SocketUser user = Context.Message.Author;
+            Discord.UserExtensions.SendMessageAsync(user, messageString).Wait();
+        }
+
+        [Command("report")]
+        public async Task Report()
+        {
+           
+            void SendDirect(SocketUser user)
+            {
+                string message = "This is just a placeholder test";
+                Discord.UserExtensions.SendMessageAsync(user, message).Wait();
+            }
+        }
+
+        [Command("Garbage")]
+        private async Task Max()
+        {
+            string maxx = "C:\\maxx.jpg";
+            await Context.Channel.SendFileAsync(maxx, "Fucking Garbage!");
+        }
+
+        [Command("Schedule")]
+        private async Task Schedule()
+        {
+            DateTime dt1 = DateTime.Now;
+            string weekschedule = "C:\\schedule.png";
+            await Context.Channel.SendFileAsync(weekschedule, "Updated: " + dt1);
+        }
+
+        [Command("flip")]
+        private async Task flip()
+        {
+            Random rdn = new Random();
+            int coin = rdn.Next(0, 2);
+
+            if (coin == 1)
+            {
+                await ReplyAsync("The answer is **heads**");
+
+            }
+            else if (coin == 0)
+            {
+                await ReplyAsync("The answer is **tails**");
+            }
+
+
+        }
+
+        [Command("makemeasandwich")]
+        async Task makemeasandwich()
+        {
+            string food = "C:\\sandwich.gif";
+            await Context.Channel.SendFileAsync(food, "Nom Nom");
+        }
+
+        [Command("catchapacket")]
+        public async Task cap()
+        {
+            await ReplyAsync("Yeah... not in overload im afriad.");
+        }
+
+        [Command("wirelesswins")]
+        public async Task ww()
+        {
+            await ReplyAsync("Use a fucking wire, asshole.");
+        }
+
+        [Command("lucky")]
+        public async Task role()
+        {
+            Random rdn = new Random();
+            int chance = rdn.Next(0, 23);
+
+            if (chance == 3)
+            {
+                var user = Context.User;
+                var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "lucky");
+                await (user as IGuildUser).AddRoleAsync(role);
+                await ReplyAsync("95.24% chance of losing. Consider a lottery ticket ;)");
+            }
+            else if (chance != 3)
+                await ReplyAsync("Sorry better luck next time.");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Sheriff
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+
+            string token = "NzM5OTIwNTcwMTA3NTU5OTk3.Xyheug.c0YcparuhayYXf_UVigLSp0nMMo";
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
+    public class MemberAssignmentService
+    {
+        private readonly ulong _roleId;
+        public MemberAssignmentService(DiscordSocketClient client, ulong roleId)
+        {
+            // Hook the evnet
+            client.UserJoined += AssignMemberAsync;
+
+            // Note that we are using role identifier here instead
+            // of name like your original solution; this is because
+            // a role name check could easily be circumvented by a new role
+            // with the exact name.
+            _roleId = roleId;
+        }
+
+        private async Task AssignMemberAsync(SocketGuildUser guildUser)
+        {
+            var guild = guildUser.Guild;
+            // Check if the desired role exist within this guild.
+            // If not, we simply bail out of the handler.
+            var role = guild.GetRole(_roleId);
+            if (role == null) return;
+            // Check if the bot user has sufficient permission
+            // Finally, we call AddRoleAsync
+            await guildUser.AddRoleAsync(role);
+        }
+    }
+}
